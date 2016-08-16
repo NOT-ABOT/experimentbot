8871aaa2b983030ac7644ebceaa0374bf8e8f86b
NOT-ABOT <notabot1010@gmail.com>
2016-08-15 21:10:17 -0500

    Update ebot.py

--- ebot.py
+++ ebot.py
@@ -1,1 +1,6 @@
-import praw
+import praw, sqlite3, time, re
+
+
+disclaimer='*I am a bot, and this was done automatically. I was created by two college kids, so please be patient with me.*'
+sub = 'freekarma4you'
+posts = 1000
